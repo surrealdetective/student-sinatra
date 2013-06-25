a26b12884e37b8a66ef9975a31d9660b9560b9c2
surrealdetective <thefifth@gmail.com>
2013-06-25 09:49:07 -0400

    fixed routing to support only one template profile.erb file

--- app.rb
+++ app.rb
@@ -2,12 +2,26 @@ require 'sinatra/base'
 require "sqlite3"
 require_relative 'lib/concerns/persistable'
 require_relative 'lib/models/student'
+# require "debugger"
 
 # set :database, "sqlite3:///student_rack.db"
 
 # Why is it a good idea to wrap our App class in a module?
 module StudentSite
   class App < Sinatra::Base
+    #@students variable defined for class
+    @students = Student.all
+    
+    #creates ERB profiles
+    @students.each do |student|
+      template = ERB.new(File.read("views/profile.erb"))
+      # debugger
+      f = File.new("views/#{student.id}.erb", "w")
+      File.open(f, "w") do |file|
+        file.write(template)
+      end
+    end
+
     get '/' do
       "hello world!"
     end
@@ -17,7 +31,7 @@ module StudentSite
       erb :students
     end
 
-    @students = Student.all
+    #creates routes for ERB profiles
     @students.each do |student|
       student_id = student.id
       get "/#{student_id}" do
